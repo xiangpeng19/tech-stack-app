98a712c08640002d73d02f61598335f0e6fc3a10
Xiangpeng Li <lxp1991@gmail.com>
2021-07-17 18:26:57 -0700

    add devise gem as auth system

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,8 @@
+# frozen_string_literal: true
+
+# home controller
 class HomeController < ApplicationController
+  def index
+    @content = User.take.last_sign_in_ip.inspect
+  end
 end
---
+++ db/migrate/20210717042000_add_trackable_lockable_to_users.rb
@@ -0,0 +1,17 @@
+class AddTrackableLockableToUsers < ActiveRecord::Migration[6.1]
+  def change
+    change_table :users do |t|
+      ## Trackable
+      t.integer  :sign_in_count, default: 0, null: false
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.string   :current_sign_in_ip
+      t.string   :last_sign_in_ip
+
+      ## Lockable
+      t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
+      t.string   :unlock_token # Only if unlock strategy is :email or :both
+      t.datetime :locked_at
+    end
+  end
+end
